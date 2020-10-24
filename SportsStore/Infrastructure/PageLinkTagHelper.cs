504d4e7083e705499c0de5f30af75ac23708b6df
Boris Korobeinikov <slon2k@gmail.com>
2020-10-24 21:20:50 +0600

    added: Pagination

---
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.ViewModels;
+
+namespace SportsStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            this.urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+
+        public string PageAction { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+        
+        public string PageClass { get; set; }
+        
+        public string PageClassNormal { get; set; }
+        
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            var result = new TagBuilder("div");
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+                tag.AddCssClass(PageClass);
+                if (PageClassesEnabled == true)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(PageModel.Page == i ? PageClassSelected : PageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result);
+        }
+
+    }
+}
